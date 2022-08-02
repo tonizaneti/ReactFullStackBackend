43e15a5017f13f6497cdc46450026770d2c43507
tonizaneti <antonio.zaneti@gmail.com>
2022-08-02 18:40:03 -0300

    Full Stack Frontend

---
+++ index.js
@@ -0,0 +1,23 @@
+import  express from "express";
+import db from "./config/database.js";
+
+
+import productRoutes from "./routes/index.js";
+
+import cors from "cors";
+
+const app = express();
+
+try {
+    await db.authenticate();
+    console.log('Database conected...');
+
+}  catch(error){
+    console.error('Connection error', error);
+}
+
+app.use(cors());
+app.use(express.json());
+app.use('/products', productRoutes);
+
+app.listen(5000,()=> console.log('Server running at port 5000'));
